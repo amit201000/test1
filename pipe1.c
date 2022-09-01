99a908798b896ea0ce3f670d82c1a2d096da4367
AMIT KUMAR SINGH <amit201000@gmail.com>
2022-09-01 14:37:20 +0530

    Add files via upload

---
+++ pipe1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+ 
+int main()
+ {
+  int pipefds[2];
+ 
+  if(pipe(pipefds) == -1) {
+    perror("pipe");
+    exit(EXIT_FAILURE);
+  }
+ 
+  printf("Read File Descriptor Value: %d\n", pipefds[0]);
+  printf("Write File Descriptor Value: %d\n", pipefds[1]);
+ 
+  return 0;
+}
